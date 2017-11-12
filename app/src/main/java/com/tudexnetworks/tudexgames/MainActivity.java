e49524d5d0c62f66980114a49f7e89e1deea74cf
Alejandro Avellaneda <Aleavellaneda1@gmail.com>
2017-11-12 01:32:09 -0300

    Carga Inicial de Tudex Games Android

---
+++ app/src/main/java/com/tudexnetworks/tudexgames/MainActivity.java
@@ -0,0 +1,37 @@
+package com.tudexnetworks.tudexgames;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.loadUrl("https://www.tudexgames.com/v3/app/");
+
+    }
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
+
