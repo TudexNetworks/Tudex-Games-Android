82484ec356f8ef238b218ca15f6c28bed78dd30f
Alejandro Avellaneda <Aleavellaneda1@gmail.com>
2017-11-26 18:11:37 -0300

    cambio de vercion de y optimizacion de codigo

--- app/src/main/java/com/tudexnetworks/tudexgames/MainActivity.java
+++ app/src/main/java/com/tudexnetworks/tudexgames/MainActivity.java
@@ -1,42 +1,25 @@
 package com.tudexnetworks.tudexgames;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("https://www.tudexgames.com/v3/app");
-
-
-
-
+        myWebView.loadUrl("https://www.tudexgames.com/app");
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-
-
-
         myWebView.setWebViewClient(new WebViewClient());
-
-
-
-
         }
-
         @Override
         public boolean onKeyDown(int keyCode, KeyEvent event) {
             // Check if the key event was the Back button and if there's history
@@ -48,17 +31,7 @@ public class MainActivity extends Activity {
             // If it wasn't the Back key or there's no web page history, bubble up to the default
             // system behavior (probably exit the activity)
             return super.onKeyDown(keyCode, event);
-
-
     }
-
-
-
-
-
-
-
-
 }
 
 
